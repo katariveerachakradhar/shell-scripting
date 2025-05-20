d0449b6e25093ff5c92a70ca3fc7094da9a123e3
Veera <veeravamshi1@gmail.com>
2025-05-20 16:59:33 +0530

    Hello

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Nana:: Hey Musk, How are you?"
-echo "N:: Hello Trump, I am fine. How are you doing"
+echo "Nana:: Hey N, How are you?"
+echo "N:: Hello Nana, I am fine. How are you doing"
 echo "Nana: I am fine too. What's up?"
 echo "N:: Nohting, just going to Mars now, will you come?"
 echo "Nana:: Sorry, you carry on! I will come once you come back"
